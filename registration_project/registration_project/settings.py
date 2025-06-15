f33ba2d8f5216733aec7f700368e8827d5268b65
selvasundari11 <selvampt8@gmail.com>
2025-06-15 14:15:00 +0300

    Update settings.py

--- registration_project/registration_project/settings.py
+++ registration_project/registration_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(4&2&3$l6^5mi%awd&9$sc2au4kukpl19=s974$67-aath^*3)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["selvasundari.pythonanywhere.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["selvasundari.pythonanywhere.com"]
 
 
 
